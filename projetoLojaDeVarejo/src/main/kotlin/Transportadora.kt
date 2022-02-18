52ca6f9efdcf8e089fa671837a223a409b0b593e
Gabriel Furtado <98998937+oGabrielFurtado@users.noreply.github.com>
2022-02-18 02:28:01 -0300

    Adicionando o Método Construtor

--- projetoLojaDeVarejo/src/main/kotlin/Transportadora.kt
+++ projetoLojaDeVarejo/src/main/kotlin/Transportadora.kt
@@ -1,6 +1,10 @@
-class Transportadora {
-    var nome: String? = null
-    var cnpj: String? = null
-    var endereco: String? = null
-    var telefoneComercial: String? = null
+class Transportadora(
+    var nome: String,
+    var cnpj: String,
+    var endereco: String,
+    var telefoneComercial: String
+) {
+    override fun toString(): String {
+        return "Nome da transportadora: $nome  Telefone: $telefoneComercial"
+    }
 }
