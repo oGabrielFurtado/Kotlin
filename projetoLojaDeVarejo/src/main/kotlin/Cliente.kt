253c86d40163755c245e1f311326d178699fe7cd
Gabriel Furtado <98998937+oGabrielFurtado@users.noreply.github.com>
2022-02-18 01:24:38 -0300

    Adicionando o Método Construtor

--- projetoLojaDeVarejo/src/main/kotlin/Cliente.kt
+++ projetoLojaDeVarejo/src/main/kotlin/Cliente.kt
@@ -1,7 +1,11 @@
-class Cliente {
-    var nome: String? = null
-    var idade: Byte? = null
-    var cpf: String? = null
-    var telefoneCelular: String? = null
-    var enderecoResidencial: String? = null
+class Cliente (
+    var nome: String,
+    var idade: Byte,
+    var cpf: String,
+    var telefoneCelular: String,
+    var enderecoResidencial: String
+) {
+    override fun toString(): String{
+        return "Nome do cliente: $nome  CPF: $cpf  Telefone para contato: $telefoneCelular"
+    }
 }
